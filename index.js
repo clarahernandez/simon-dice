2e19594776ab0baf52f06a25ec2be997999d0e64
Clara Hernández <clara.herni@gmail.com>
2020-05-28 20:55:17 -0300

    andan los colores
    Los colores cambian con el pasar del tiempo y vuelven a su estado normal

--- index.js
+++ index.js
@@ -1,24 +1,71 @@
+const INTENTOS = 10;
+const COLORES = ['rojo', 'verde', 'amarillo', 'azul'];
 
 document.querySelector('#boton-empezar').onclick = function (event) {
     ocultarBotonEmpezar();
     mostrarBotonReiniciar();
 
+    empezar();
     event.preventDefault();
-}
+};
 
 function ocultarBotonEmpezar() {
-    document.querySelector("#boton-empezar").className = "btn btn-outline-success oculto"
+    document.querySelector('#boton-empezar').className = 'btn btn-outline-success oculto';
 }
 
 function mostrarBotonReiniciar() {
-    document.querySelector("#boton-reiniciar").className = "btn btn-outline-danger"
+    document.querySelector('#boton-reiniciar').className = 'btn btn-outline-danger';
 }
 
+function empezar() {
+    let simonDice = []; //lista de colores
+    simonDice = agregarPaso(simonDice);
+    simonDice = agregarPaso(simonDice);
+    reproducir(simonDice);
+}
 
+function agregarPaso(lista) {
+    const numeroRandom = Math.floor(Math.random() * 4);
+    const color = COLORES[numeroRandom];
 
+    lista.push(color);
+    return lista;
+}
+
+function reproducir(lista) {
+    let idTime = setInterval(function () {
+        mostrarColores(lista);
+    }, 3000);
+
+    console.log(lista);
+
+    let i = 0;
+    function mostrarColores(lista) {
+        console.log(lista[0]);
+        let $color = document.querySelector(`#${lista[i]}`);
+
+        if (i === lista.length) {
+            clearInterval(idTime);
+        } else {
+            $color.classList.replace(`${lista[i]}-apagado`, `${lista[i]}-prendido`);
+            setTimeout(function () {
+                $color.classList.replace(`${lista[i]}-prendido`, `${lista[i]}-apagado`);
+                i++;
+            }, 2000);
+        }
+    }
+}
 
 function imprimirLista() {
     let lista = [1, 2, 3, 4];
-    let largo = lista.length;
+    let recorrido = 0;
     let idTime = setInterval(imprimir, 3000);
+    function imprimir() {
+        if (recorrido === lista.length) {
+            clearInterval(idTime);
+        } else {
+            recorrido++;
+            console.log(recorrido);
+        }
+    }
 }
