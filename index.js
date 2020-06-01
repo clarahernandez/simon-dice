6057051831ea02b09b84c372adbe652f3c71ba6c
Clara Hernández <clara.herni@gmail.com>
2020-06-01 12:12:23 -0300

    arreglo de funciones
    Ahora uso foreach para mostrar las listas de colores. Modularicé un poco para que sea más claro

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const INTENTOS = 10;
+const NIVELES_MAX = 5;
 const COLORES = ['rojo', 'verde', 'amarillo', 'azul'];
 
 document.querySelector('#boton-empezar').onclick = function (event) {
@@ -9,19 +9,42 @@ document.querySelector('#boton-empezar').onclick = function (event) {
     event.preventDefault();
 };
 
-function ocultarBotonEmpezar() {
-    document.querySelector('#boton-empezar').className = 'btn btn-outline-success oculto';
-}
-
-function mostrarBotonReiniciar() {
-    document.querySelector('#boton-reiniciar').className = 'btn btn-outline-danger';
-}
+document.querySelector('#boton-reiniciar').onclick = function (event) {
+    ocultarBotonReiniciar();
+    mostrarBotonEmpezar();
+    event.preventDefault();
+};
 
 function empezar() {
-    let simonDice = []; //lista de colores
-    simonDice = agregarPaso(simonDice);
-    simonDice = agregarPaso(simonDice);
-    reproducir(simonDice);
+    let simonDice = []; //lista de colores a seguir
+    let numeroNivel = 0;
+    let acerto = true;
+    while (numeroNivel < NIVELES_MAX && acerto) {
+        simonDice = agregarPaso(simonDice);
+        reproducir(simonDice);
+        acerto = turnoJugador(simonDice);
+    }
+}
+
+function turnoJugador(simonDice) {
+    console.log('turno del jugador');
+    let jugadorDice = [];
+    let acerto = true;
+    for (let i = 0; acerto && i < simonDice.length; i++) {
+        jugadorDice = agregaColorJugador(jugadorDice);
+        acerto = simonDice[i] === jugadorDice[i]; //Nos fijamos que lo que haya puesto el jugador sea igual a lo que simón dice.
+    }
+    return acerto;
+}
+
+function agregaColorJugador(lista) {
+    document.querySelectorAll('.cuadro').forEach(function (cuadro) {
+        cuadro.addEventListener('click', function () {
+            lista.push(cuadro.id);
+        });
+    });
+    console.log(lista);
+    return lista;
 }
 
 function agregarPaso(lista) {
@@ -33,39 +56,34 @@ function agregarPaso(lista) {
 }
 
 function reproducir(lista) {
-    let idTime = setInterval(function () {
-        mostrarColores(lista);
-    }, 3000);
-
-    console.log(lista);
-
-    let i = 0;
-    function mostrarColores(lista) {
-        console.log(lista[0]);
-        let $color = document.querySelector(`#${lista[i]}`);
-
-        if (i === lista.length) {
-            clearInterval(idTime);
-        } else {
-            $color.classList.replace(`${lista[i]}-apagado`, `${lista[i]}-prendido`);
-            setTimeout(function () {
-                $color.classList.replace(`${lista[i]}-prendido`, `${lista[i]}-apagado`);
-                i++;
-            }, 2000);
-        }
-    }
+    lista.forEach(function ($cuadro, index) {
+        const RETRASO = (index + 1) * 1000;
+        setTimeout(function () {
+            resaltar($cuadro);
+        }, RETRASO);
+    });
 }
 
-function imprimirLista() {
-    let lista = [1, 2, 3, 4];
-    let recorrido = 0;
-    let idTime = setInterval(imprimir, 3000);
-    function imprimir() {
-        if (recorrido === lista.length) {
-            clearInterval(idTime);
-        } else {
-            recorrido++;
-            console.log(recorrido);
-        }
-    }
+function resaltar(cuadro) {
+    let $cuadro = document.querySelector(`#${cuadro}`);
+    $cuadro.classList.replace(`${cuadro}-apagado`, `${cuadro}-prendido`);
+    setTimeout(function () {
+        $cuadro.classList.replace(`${cuadro}-prendido`, `${cuadro}-apagado`);
+    }, 1000);
+}
+
+function ocultarBotonEmpezar() {
+    document.querySelector('#boton-empezar').className = 'btn btn-outline-success oculto';
+}
+
+function mostrarBotonReiniciar() {
+    document.querySelector('#boton-reiniciar').className = 'btn btn-outline-danger';
+}
+
+function mostrarBotonEmpezar() {
+    document.querySelector('#boton-empezar').className = 'btn btn-outline-success';
+}
+
+function ocultarBotonReiniciar() {
+    document.querySelector('#boton-reiniciar').className = 'btn btn-outline-danger oculto';
 }
